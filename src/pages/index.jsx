f67fa4dc4fb96e09f7925e891a9c87fc27bbd872
Victor Mesquita <contato@victormesquita.dev>
2020-04-11 20:23:36 -0300

    fix(theme): Adicionado busca ao theme armazenado no localstorage ao carregar a pagina index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,20 @@
-import React from "react"
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import Projects from "../components/projects"
 import { useStaticQuery, graphql } from "gatsby"
 import socialMedias from "../data/social_media.yml"
 import projects from "../data/projects.yml"
+import useThemeContext from "../hooks/themeContext"
 
 import "../../sass/style.scss"
 
 export default () => {
+  const themeContext = useThemeContext()
+
+  useEffect(() => {
+    const saved_theme = localStorage.getItem("theme")
+    themeContext.setTheme(saved_theme)
+  }, [])
 
   const {
     github: { viewer: user },

99617743b6937039a1273470f2cc81cb712e1c58
victor-mesquita <victor-mesquita@users.noreply.github.com>
2021-02-11 01:02:37 +0000

    deploy: 27b859605b5e38abcb2987c9caa64b2d96436f7a

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a46be5c8f9e7fb1ccc76.js"
+    "url": "webpack-runtime-3d7eaf901c678152119b.js"
   },
   {
     "url": "framework-8625eb27c510615d831b.js"
@@ -40,7 +40,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "6f681c4c837d39a07a20a32e59a84102"
+    "revision": "c8d637468ade0f52635be7d4bfb15523"
   },
   {
     "url": "styles.b8b080cefc6ef4479d11.css"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
     "url": "styles-c2fe8482057191dca484.js"
   },
   {
-    "url": "component---src-pages-index-jsx-ce66b895b54c1b4b7361.js"
+    "url": "component---src-pages-index-jsx-d69903af72c32f493296.js"
   },
   {
     "url": "759d719527ed880a99706b5cd9cf7ac0b3435dfd-fe60b5dac3d80f9a94fe.js"
@@ -60,7 +60,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "ab107d56a7c5583484c6ac5508e9bc49"
+    "revision": "fc46152839a934818f5b9f6e9ecdd8f3"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
